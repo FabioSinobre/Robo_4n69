febfb02f9cc0826f331a5873f9da3d225b4c7e9b
FabioSinobre <engenharia.computacao4.0@gmail.com>
2021-10-16 08:29:00 -0300

    Project creatd

---
+++ backend/src/main/java/com/sinobre/robo_4n69/Robo4n69Application.java
@@ -0,0 +1,13 @@
+package com.sinobre.robo_4n69;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Robo4n69Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Robo4n69Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sinobre/robo_4n69/Robo4n69ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinobre.robo_4n69;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Robo4n69ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
