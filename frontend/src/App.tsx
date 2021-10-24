106f90806a49b261d8d5d5689bccfd7234167fdc
FabioSinobre <engenharia.computacao4.0@gmail.com>
2021-10-24 20:14:35 -0300

    Up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import Article from "components/Article";
 import Carousel from "components/Carousel";
 import Footer from "components/Footer";
-import Header from "components/Header";
 import Login from "components/Login";
 import NavBar from "components/NavBar";
 import Section from "components/Section";
--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,25 +1,22 @@
+
 function NavBar() {
   return (
+    <>
     <div className="conteiner navBar" >
-
-      
       <ul className="nav justify-content-end ">
       <header><h1 className="text-robo-1 space">Robô 4n69</h1></header>
         <li className="nav-item ">
-          <a className="nav-link active" aria-current="page" href="#">Active</a>
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
+          <a className="nav-link" href="#">Quem somos</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
+          <a className="nav-link" href="#">Robô</a>
         </li>
       </ul>
     </div>
+    </>
   );
 }
-
 export default NavBar;
