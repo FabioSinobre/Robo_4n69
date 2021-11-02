86bdb9d9deacd13a730213b3c16d1da768b5ec56
FabioSinobre <engenharia.computacao4.0@gmail.com>
2021-11-02 11:24:54 -0300

    Routes Katia

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
       <div className="conteiner">
        <div className="footer text-center bg-primary text-white">
-           <p>Desenvolvido por Alunos da UNIVESP</p>
+           <p>Desenvolvido por Alunos da UNIVESP(Univesidade Virtual Do Estado de São Paulo</p>
            <p>Engenharia da Computação | São Paulo - outubro de 2021</p>
        </div>
       </div>
